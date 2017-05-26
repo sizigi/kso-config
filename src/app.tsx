f35599af0ebfd752b3650d42a0fbdfa1f94227ee
Cory Li <coryli@mit.edu>
2017-05-26 12:33:12 -0700

    blueprint installed

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
+
+import { Button } from '@blueprintjs/core';
 import { Hotkey } from './hotkey';
 
 export class App extends React.Component<undefined, undefined> {
   render() {
     return (
-      <Hotkey />
+      <div>
+        <Hotkey />
+        <Button>Hello World</Button>
+      </div>
     );
   }
 }
