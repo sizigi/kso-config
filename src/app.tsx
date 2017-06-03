e381506c31f21f80b36170a89534eb219b7bbef5
Cory Li <coryli@mit.edu>
2017-06-02 18:59:16 -0700

    some transitions and extra text

--- src/app.tsx
+++ src/app.tsx
@@ -1,28 +1,98 @@
 import * as classNames from 'classNames';
 import * as React from 'react';
+import DevTools from 'mobx-react-devtools';
+import {
+  action,
+  observable,
+  ObservableMap,
+  whyRun
+} from 'mobx';
 import { Button, EditableText } from '@blueprintjs/core';
 import {
   Colors,
   FocusStyleManager,
   Intent,
   NonIdealState,
+  Dialog,
   Tab2,
   Tabs2
 } from '@blueprintjs/core';
+import { CSSTransitionGroup } from 'react-transition-group';
+import { Device, devices, HID } from 'node-hid';
 import { HotkeyInput } from './hotkey';
 import { MainToaster } from './toaster';
 import { observer } from 'mobx-react';
 import { SingleHidKeyReport } from './hidreport';
 import { SingleKeyReportEditor } from './singlekeyreporteditor';
 
-
 FocusStyleManager.onlyShowFocusOnTabs();
+
+export class KeyData {
+  @observable name: string;
+  @observable report: SingleHidKeyReport;
+  @observable serial: string;
+  @observable firmware: string;
+  @observable address: string;
+  @observable path: string;
+
+  constructor(device: Device) {
+    this.name = device.product;
+    this.report = new SingleHidKeyReport();
+    this.serial = device.serialNumber;
+    this.firmware = 'Unknown';
+    this.address = '0xA1';
+    this.path = device.path;
+  }
+
+  static isSupported(device: Device): boolean {
+    return /USB/.test(device.product);
+  }
+}
+
+export class AppStore {
+  @observable devices: ObservableMap<KeyData>;
+  constructor() {
+    this.devices = observable.map<KeyData>();
+  }
+
+  @action.bound public updateDevices(): void {
+    console.log('updating...');
+    let allHidDevices = devices();
+
+    this.devices.clear();
+
+    for (let d of allHidDevices) {
+      if (!KeyData.isSupported(d)) continue;
+      this.devices.set(d.path, new KeyData(d));
+    }
+  }
+
+}
 
 let keyReport = new SingleHidKeyReport();
 
 @observer
 export class App extends React.Component<undefined, undefined> {
+  private store: AppStore;
+
+  constructor() {
+    super()
+    this.store = new AppStore();
+  }
+
   render() {
+    const items = this.store.devices.values().map((device) => {
+      return (
+        <div key={device.path} className={classNames('kso-tab', { 'active': false })}>
+          <img src="http://placehold.it/48x48" style={{ borderRadius: 48, marginRight: 20 }}></img>
+          <div style={{ paddingTop: 10 }}>
+            <h4>{device.name}</h4>
+            <h6 className="pt-text-muted">Key: F1</h6>
+          </div>
+        </div>
+      )
+    });
+
     return (
       <div
         style={{
@@ -31,42 +101,40 @@ export class App extends React.Component<undefined, undefined> {
           display: 'flex',
         }}
       >
+        <DevTools />
         <nav style={{ flex: 1, backgroundColor: Colors.LIGHT_GRAY1, height: '100vh' }}>
           <div className='pt-navbar pt-dark kso-navbar'>
             <div className="pt-navbar-group pt-align-left">
               <div className="pt-navbar-heading pt-text-muted">keyswitch.one</div>
             </div>
             <div className="pt-navbar-group pt-align-right">
-              <button className="pt-button pt-minimal pt-icon-refresh"></button>
-              {/*<button className="pt-button pt-minimal pt-icon-cog"></button>*/}
+              <button
+                className="pt-button pt-minimal pt-icon-refresh"
+                onClick={this.store.updateDevices}
+              />
             </div>
           </div>
           <div style={{ height: 'calc(100vh - 75px)' }} data-simplebar>
-            <div className="p-t-lg">
-              <NonIdealState
-                visual="warning-sign"
-                title="No devices found"
-                description="No supported devices were detected. Try plugging one in." />
-            </div>
-            {/*<div className="kso-tab active">
-              <img src="http://placehold.it/48x48" style={{ borderRadius: 48, marginRight: 20 }}></img>
-              <div style={{ paddingTop: 10 }}>
-                <h4>Help Key</h4>
-                <h6 className="pt-text-muted">Key: F1</h6>
-              </div>
-            </div>
-            <div className="kso-tab">
-              <img src="http://placehold.it/48x48" style={{ borderRadius: 48, marginRight: 20 }}></img>
-              <div style={{ paddingTop: 10 }}>
-                <h4>Help Key</h4>
-                <h6 className="pt-text-muted">Key: F1</h6>
-              </div>
-            </div>*/}
+            <CSSTransitionGroup
+              transitionName="example"
+              transitionEnterTimeout={500}
+              transitionLeaveTimeout={500}>
+              {items}
+            </CSSTransitionGroup>
+            {items.length === 0
+              ? (<div className="p-t-lg">
+                <NonIdealState
+                  visual="warning-sign"
+                  title="No devices found"
+                  description="No supported devices were detected. Try plugging one in." />
+              </div>)
+              : <div></div>}
           </div>
         </nav>
 
         <section style={{ flex: 2 }}>
-          <div style={{ padding: 20 }}>
+          <div id='title-bar' />
+          <div style={{ padding: 20, paddingTop: 0 }}>
             <Button
               style={{ float: "right" }}
               text="Save"
@@ -98,6 +166,26 @@ export class App extends React.Component<undefined, undefined> {
               <Tab2 id="macro" title="Macro" />
               <Tabs2.Expander />
               <Button iconName="help" className="pt-intent-primary pt-minimal" />
+              <Dialog iconName="help" isOpen={true} title="Help">
+                <div className="pt-dialog-body">
+                  <h5>Keypress</h5>
+                  <p>The keypress setting sends a keydown when the switch is depressed,
+                     and a keyup when the switch is released, just like a normal keyboard.
+                  </p>
+                  <h5>Text Expand</h5>
+                  <p>After press and release, the switch will transmit the entire string.
+                    Unicode is transmitted via unicode codes on windows.
+                  </p>
+                </div>
+                <div className="pt-dialog-footer">
+                  <div className="pt-dialog-footer-actions">
+                    <Button
+                      intent={Intent.PRIMARY}
+                      text="OK"
+                    />
+                  </div>
+                </div>
+              </Dialog>
             </Tabs2>
           </div>
         </section>
